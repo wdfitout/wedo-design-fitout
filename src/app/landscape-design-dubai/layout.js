11ab6187d531a4310b70a2d2b1ec28b573f65791
shum-aiza <shumaizat@gmail.com>
2025-07-29 17:32:37 +0500

    landscape page metas update

--- src/app/landscape-design-dubai/layout.js
+++ src/app/landscape-design-dubai/layout.js
@@ -1,9 +1,9 @@
 import '../globals.css';
 
 export const metadata = {
-  title: "Apartment Interior Design Dubai - Modern Interior Design",
+  title: "Luxury Landscape Company in Dubai - Landscape Design Dubai ",
   description:
-    "Luxury apartment interior design in Dubai by WE DO. Modern, classic, or creative styles, beautiful spaces made for you. Call now to start your project!",
+    "Enhance your outdoors with expert landscape design in Dubai by WE DO Interior Design & Fit-Out. We offer villa landscaping, garden design, and outdoor solutions across Dubai and the UAE.  Call Now.",
   metadataBase: new URL('https://wedointerior.ae/'),
   alternates: {
     canonical: '/apartment-interior-design-dubai',
