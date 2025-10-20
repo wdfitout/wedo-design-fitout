6932517ef2419c68d7c0d362fe45f1ae97474d30
wild-alpha <digitalaviner@gmail.com>
2025-10-20 16:11:41 +0400

    primo2

--- src/app/ii-primo-penthouse/VHeroSection.jsx
+++ src/app/ii-primo-penthouse/VHeroSection.jsx
@@ -5,17 +5,18 @@ import Image from "next/image";
 
 const VHeroSection = () => {
   return (
-    <div className="relative w-full h-[300px] sm:h-[300px] md:h-[3500px] lg:h-[400px] xl:h-[500px] overflow-hidden">
+    <div className="relative w-full h-[300px] sm:h-[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] overflow-hidden">
       {/* Background Image */}
       <Image
         src="/images/primo-lounge.jpg"
-        alt="interior design"
+        alt="Luxury penthouse lounge interior design"
         fill
         priority
         className="object-cover"
       />
 
-   
+      {/* Optional overlay (for text or dark filter) */}
+      {/* <div className="absolute inset-0 bg-black/30" /> */}
     </div>
   );
 };
